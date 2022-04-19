f96af75f87c2a6cb75d9e5c05c09d47ad03552ca
Cid-Jie <pcidjie@gmail.com>
2022-04-19 11:24:57 +0200

    add files to listing all musics and add header nav with css

--- src/Controller/MusicController.php
+++ src/Controller/MusicController.php
@@ -6,11 +6,14 @@ use App\Model\MusicManager;
 
 class MusicController extends AbstractController
 {
+    /**
+     * List musics
+     */
     public function showList()
     {
         $musicManager = new MusicManager();
         $musicals = $musicManager->musicByGenre();
 
-        return $this->twig->render('Music/list.html.twig', ['musicals' => $musicals]);   
-    }   
+        return $this->twig->render('Music/list.html.twig', ['musicals' => $musicals]);
+    }
 }
--- src/Model/MusicManager.php
+++ src/Model/MusicManager.php
@@ -6,12 +6,13 @@ class MusicManager extends AbstractManager
 {
     public const TABLE = 'music';
 
+    /**
+     * Get all informations for musics from database.
+     */
     public function musicByGenre()
     {
         $query = 'SELECT music.title, music.author, music.source, genre.genre_name 
-        FROM music 
-        INNER JOIN musical_genre AS genre ON music.musical_genre_id=genre.id 
-        ORDER BY genre.genre_name';
+        FROM music INNER JOIN musical_genre AS genre ON music.musical_genre_id=genre.id;';
 
         return $this->pdo->query($query)->fetchAll();
     }
