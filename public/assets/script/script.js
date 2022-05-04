bf3f95ad6297203deb51354d2fea5d426442e044
jugets <justine.gets@posteo.net>
2022-05-04 12:22:58 +0200

    Modification of the genre filter for the mobile interface

--- public/assets/script/script.js
+++ public/assets/script/script.js
@@ -1,11 +1,13 @@
+//buttons = tableau à 2 dimensions, 1 dimension de genre et 1 dimension d'interface (2 boutons par genre)
+
 let buttons = [
-document.getElementById("pop"),
-document.getElementById("cinematic"),
-document.getElementById("electro"),
-document.getElementById("world"),
-document.getElementById("chillout"),
-document.getElementById("ambient"),
-document.getElementById("all")
+document.querySelectorAll("#pop"),
+document.querySelectorAll("#cinematic"),
+document.querySelectorAll("#electro"),
+document.querySelectorAll("#world"),
+document.querySelectorAll("#chillout"),
+document.querySelectorAll("#ambient"),
+document.querySelectorAll("#all")
 ]
 
 let cards = [
@@ -17,23 +19,29 @@ document.querySelectorAll("#music-card-chillout"),
 document.querySelectorAll("#music-card-ambient")
 ]
 
-for (let i = 0; i < buttons.length; i++) {
-    const button = buttons[i];
-    button.addEventListener('click', () => {
-    if( button.id != 'all') {
-        buttons[6].classList.remove('d-none');
-    } else {
-        buttons[6].classList.add('d-none');
-    }
-    for (let x = 0; x < cards.length; x++) {
-        for (let y = 0; y < cards[x].length; y++) {
-            const card = cards[x][y];
-            console.log(card);
-            card.classList.remove('d-none');
-            if (i != 6 && card.id != cards[i][0].id) {
-                card.classList.add('d-none');
+const desktop = 0;
+const mobile = 1;
+const interfaces = [desktop,mobile];
+
+interfaces.forEach(interface => {
+    for (let btnIndex = 0; btnIndex < buttons.length; btnIndex++) {
+        const button = buttons[btnIndex][interface];
+        button.addEventListener('click', () => {
+            if( button.id != 'all' && interface == desktop) {
+                buttons[6][desktop].classList.remove('d-none');
+            } else {
+                buttons[6][desktop].classList.add('d-none');
             }
-        }
+            for (let genreIndex = 0; genreIndex < cards.length; genreIndex++) {
+                for (let musicIndex = 0; musicIndex < cards[genreIndex].length; musicIndex++) {
+                    const card = cards[genreIndex][musicIndex];
+                    console.log(card);
+                    card.classList.remove('d-none');
+                    if (btnIndex != 6 && card.id != cards[btnIndex][0].id) {
+                        card.classList.add('d-none');
+                    }
+                }
+            }
+        })
     }
-})
-}
+});
